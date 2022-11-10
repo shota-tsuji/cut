a8dfb60753d1cfb70fcca120900e1e0df4580f12
shota-tsuji <shotatuzi@gmail.com>
2022-11-10 23:59:40 +0900

    feat: typescript is used

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    // メインとなるJavaScriptファイル（エントリーポイント）
+    mode: process.env.NODE_ENV || "development",
+    entry: {
+        popup: "./src/index.ts",
+    },
+    output: {
+        path: `${__dirname}/dist/js`,
+        filename: "[name].js",
+    },
+    devServer: {
+        static: "dist",
+        open: true
+    },
+    devtool: 'cheap-module-source-map',
+    plugins: [
+        new CopyPlugin({
+            patterns: [{ from: ".", to: "../", context: "public" }],
+            options: {},
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js',],
+    },
+};
