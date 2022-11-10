74c4a5700c1340784ee2f8352e01ecb7733e845f
shota-tsuji <shotatuzi@gmail.com>
2022-11-10 21:33:27 +0900

    feat: url and title are copied as markdown format

--- alert.js
+++ alert.js
@@ -1,7 +1,8 @@
-async function showAlert() {
+async function popup() {
     let queryOptions = { active: true, lastFocusedWindow: true };
     let [tab] = await chrome.tabs.query(queryOptions);
-    await navigator.clipboard.writeText(tab.url);
+    const text = `[${tab.title}](${tab.url})`;
+    await navigator.clipboard.writeText(text);
 }
 
-document.getElementById("alertButton").addEventListener("click", showAlert);
+document.getElementById("alertButton").addEventListener("click", popup);
