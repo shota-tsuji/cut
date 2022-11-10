d294755501b2039452c4b2bdcfc6d16b593365e5
shota-tsuji <shotatuzi@gmail.com>
2022-11-11 07:44:54 +0900

    feat: pageInfo class is created

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -1,9 +1,24 @@
 import * as React from "react";
 
+class PageInfo {
+    url: string = '';
+    title: string = '';
+
+    constructor(tab: chrome.tabs.Tab) {
+        if (!!tab.url) {
+            this.url = tab.url;
+        }
+        if (!!tab.title) {
+            this.title = tab.title;
+        }
+    }
+}
+
 async function copyForMarkdown() {
     let queryOptions = { active: true, lastFocusedWindow: true };
     let [tab] = await chrome.tabs.query(queryOptions);
-    const text = `[${tab.title}](${tab.url})`;
+    const pageInfo = new PageInfo(tab);
+    const text = `[${pageInfo.title}](${pageInfo.url})`;
     await navigator.clipboard.writeText(text);
 }
 
