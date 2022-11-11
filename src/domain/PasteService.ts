151ee59e28ed706da98042f1e6c12d7d2efedc01
shota-tsuji <shotatuzi@gmail.com>
2022-11-12 07:00:02 +0900

    refactor: functions are added to class member

--- src/domain/PasteService.ts
+++ src/domain/PasteService.ts
@@ -1,18 +1,18 @@
 import ChromeTabRepository from "../infrastructure/ChromeTabRepository";
 import PageInfo from "./PageInfo";
 
-async function copyForMarkdown() {
-    const pageInfoRepository = new ChromeTabRepository();
-    const pageInfoDto = await pageInfoRepository.getPageInfo()
-    const pageInfo = new PageInfo(pageInfoDto.url, pageInfoDto.title);
-    await navigator.clipboard.writeText(pageInfo.getMarkdownText());
-}
+export default class PasteService {
+    async copyForMarkdown() {
+        const pageInfoRepository = new ChromeTabRepository();
+        const pageInfoDto = await pageInfoRepository.getPageInfo()
+        const pageInfo = new PageInfo(pageInfoDto.url, pageInfoDto.title);
+        await navigator.clipboard.writeText(pageInfo.getMarkdownText());
+    }
 
-async function copyForJira() {
-    const pageInfoRepository = new ChromeTabRepository();
-    const pageInfoDto = await pageInfoRepository.getPageInfo()
-    const pageInfo = new PageInfo(pageInfoDto.url, pageInfoDto.title);
-    await navigator.clipboard.writeText(pageInfo.getJiraText());
+    async copyForJira() {
+        const pageInfoRepository = new ChromeTabRepository();
+        const pageInfoDto = await pageInfoRepository.getPageInfo()
+        const pageInfo = new PageInfo(pageInfoDto.url, pageInfoDto.title);
+        await navigator.clipboard.writeText(pageInfo.getJiraText());
+    }
 }
-
-export {copyForJira, copyForMarkdown};
--- src/popup.tsx
+++ src/popup.tsx
@@ -2,13 +2,15 @@ import * as React from "react";
 import { createRoot } from "react-dom/client";
 import Popup from "./components/Popup";
 import './popup.css';
-import {copyForJira, copyForMarkdown} from "./domain/PasteService";
+import PasteService from "./domain/PasteService";
 
 class PopupService  {
     private readonly html: HTMLElement | null;
+    private readonly pasteService: PasteService;
 
     constructor() {
         this.html = document.getElementById("root");
+        this.pasteService = new PasteService();
     }
 
     main() {
@@ -16,8 +18,8 @@ class PopupService  {
             const root = createRoot(this.html);
             root.render(
                 <React.StrictMode>
-                    <Popup name="Copy for Markdown" func={copyForMarkdown}/>
-                    <Popup name="Copy for Jira" func={copyForJira}/>
+                    <Popup name="Copy for Markdown" func={this.pasteService.copyForMarkdown}/>
+                    <Popup name="Copy for Jira" func={this.pasteService.copyForJira}/>
                 </React.StrictMode>,
             );
         }
