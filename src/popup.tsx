3571da54ea30522ad2f767d87c0464199c81eab0
shota-tsuji <shotatuzi@gmail.com>
2022-11-12 06:50:18 +0900

    refactor: main functions are moved to a class

--- src/popup.tsx
+++ src/popup.tsx
@@ -2,22 +2,7 @@ import * as React from "react";
 import { createRoot } from "react-dom/client";
 import Popup from "./components/Popup";
 import './popup.css';
-import PageInfo from "./domain/PageInfo";
-import ChromeTabRepository from "./infrastructure/ChromeTabRepository";
-
-async function copyForMarkdown() {
-    const pageInfoRepository = new ChromeTabRepository();
-    const pageInfoDto = await pageInfoRepository.getPageInfo()
-    const pageInfo = new PageInfo(pageInfoDto.url, pageInfoDto.title);
-    await navigator.clipboard.writeText(pageInfo.getMarkdownText());
-}
-
-async function copyForJira() {
-    const pageInfoRepository = new ChromeTabRepository();
-    const pageInfoDto = await pageInfoRepository.getPageInfo()
-    const pageInfo = new PageInfo(pageInfoDto.url, pageInfoDto.title);
-    await navigator.clipboard.writeText(pageInfo.getJiraText());
-}
+import {copyForJira, copyForMarkdown} from "./domain/PasteService";
 
 class PopupService  {
     private readonly html: HTMLElement | null;
