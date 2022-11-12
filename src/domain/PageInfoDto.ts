e0e7a36946c91ab76bf6649e7aff18e07aebe632
shota-tsuji <shotatuzi@gmail.com>
2022-11-13 02:04:13 +0900

    refactor: unused class is removed

--- src/domain/PageInfoDto.ts
+++
@@ -1,9 +0,0 @@
-export default class PageInfoDto {
-    url: string;
-    title: string;
-
-    constructor(url: string, title: string) {
-        this.url = url;
-        this.title = title;
-    }
-}
